f4e54db21286ddfa312ee397514b2c74a23afaa3
Aviral K <kainturarenu@gmail.com>
2025-06-29 17:51:13 +0530

    feat(frontend): added router for teens

--- frontend/src/router/index.ts
+++ frontend/src/router/index.ts
@@ -3,12 +3,20 @@ import { createRouter, createWebHistory } from 'vue-router'
 // Landing page
 import LandingPage from '@/views/LandingPage.vue'
 import ChildLayout from '@/layouts/ChildLayout.vue'
+import TeenLayout from '@/layouts/TeenLayout.vue'
 
 // Child views
 import ChildDashboard from '@/views/ChildDashboard.vue'
 import ChildGames from '@/views/ChildGames.vue'
 import ChildSavings from '@/views/ChildSavings.vue'
 import ChildGoals from '@/views/ChildGoals.vue'
+
+// Teen views
+import TeenDashboard from '@/views/TeenDashboard.vue'
+import TeenBudget from '@/views/TeenBudget.vue'
+import TeenGoals from '@/views/TeenGoals.vue'
+import TeenActivities from '@/views/TeenActivities.vue'
+import TeenExplore from '@/views/TeenExplore.vue'
 
 const routes = [
   {
@@ -41,6 +49,39 @@ const routes = [
         path: 'goals',
         name: 'ChildGoals',
         component: ChildGoals
+      }
+    ]
+  },
+  {
+    path: '/teen',
+    component: TeenLayout,
+    //meta: { requiresAuth: true, role: 'teen' }, #To be added once auth is setup
+    redirect: '/teen/dashboard',
+    children: [
+      {
+        path: 'dashboard',
+        name: 'TeenDashboard',
+        component: TeenDashboard
+      },
+      {
+        path: 'budget',
+        name: 'TeenBudget',
+        component: TeenBudget
+      },
+      {
+        path: 'goals',
+        name: 'TeenGoals',
+        component: TeenGoals
+      },
+      {
+        path: 'activities',
+        name: 'TeenActivities',
+        component: TeenActivities
+      },
+      {
+        path: 'explore',
+        name: 'TeenExplore',
+        component: TeenExplore
       }
     ]
   }
