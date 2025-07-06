82503c02ffbce8145355a90e19bbd084de0ba477
Yash Agrawal <yash4agr@gmail.com>
2025-07-06 14:12:20 +0530

    fix: removes duplicate import

--- frontend/src/main.ts
+++ frontend/src/main.ts
@@ -2,7 +2,6 @@ import { createApp } from 'vue'
 import { createPinia } from 'pinia'
 import App from '@/App.vue'
 import router from '@/router'
-import App from '@/App.vue'
 import vuetify from '@/plugins/vuetify'
 
 import '@/style.css'
